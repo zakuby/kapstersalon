aeb3276ddafbddf97ac560bab8af7b8d4315228c
Zakuby <zakuby02@gmail.com>
2017-01-30 11:58:51 +0700

    update Nota

--- application/views/makeNota.php
+++ application/views/makeNota.php
@@ -108,10 +108,10 @@ $tb1 .= '<br>Payment Type&nbsp;: '.$payment_method;
 $tb1 .= '<br>Tanggal Transaksi&nbsp;: '.date("d F Y");
 $tb1 .= '<br><br>
 			<table border="0">
-                <tr style="border-top:1pt solid black;">
-                  <th>Nama Product</th>
-				  <th>Category</th>
-				  <th>Harga</th>
+                <tr>
+                  <th style="border-top:1pt solid black;">Nama Product</th>
+				  <th style="border-top:1pt solid black;">Category</th>
+				  <th style="border-top:1pt solid black;">Harga</th>
                 </tr>';
 $product_explode = explode(",",$product);
 $harga_explode = explode(",",$harga);
